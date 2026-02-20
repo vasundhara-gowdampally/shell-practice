7bafbe8a35e7e8d4a9e68e2e9249cf9d0e0efebb
vasundhara-gowdampally <vasundhara.gowdampally@gmail.com>
2026-02-20 10:46:03 -0800

    changed something

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
 echo "Script executed at $TIMESTAMP"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executed in: $TOTAL_TIME"
