64ce3bbd4db094ec3e284e5f681fd79a666750fe
vasundhara-gowdampally <vasundhara.gowdampally@gmail.com>
2026-02-20 13:29:45 -0800

    changed something

--- 10-installations.sh
+++ 10-installations.sh
@@ -10,3 +10,10 @@ fi
 
 echo "Installing Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... Failure"
+    exit 1
+else 
+    echo "Installing Nginx ... Success"
+fi        
