67ca4a4073315ce8a57f52564d098a537ae5d7c2
vasundhara-gowdampally <vasundhara.gowdampally@gmail.com>
2026-02-20 11:04:04 -0800

    changed something

---
+++ 07-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+### Special variables ###
+
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $pwd"
+echo "who is running: $user"
+echo "Home directory of the current user: $HOME"
+echo "PID of the script: $$"
+sleep 100 &
+echo "PID of the recently executed background process: $!" 
+echo "All args passed to script: $*"
