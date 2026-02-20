80e62bab302d62c7e6d33509d8ef1826e642643e
vasundhara-gowdampally <vasundhara.gowdampally@gmail.com>
2026-02-20 11:07:55 -0800

    changed something

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,8 +6,8 @@
 echo "All args passed to script: $@"
 echo "Number of vars passed to script: $#"
 echo "Script name: $0"
-echo "Present directory: $pwd"
-echo "who is running: $user"
+echo "Present directory: $PWD"
+echo "who is running: $USER"
 echo "Home directory of the current user: $HOME"
 echo "PID of the script: $$"
 sleep 100 &
