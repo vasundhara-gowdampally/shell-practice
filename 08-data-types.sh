4a5d31f28b388e9e99b9eb9a1fec4cc10c7dbd7c
vasundhara-gowdampally <vasundhara.gowdampally@gmail.com>
2026-02-20 12:09:11 -0800

    changed something

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -11,3 +11,6 @@ echo "Sum is: $SUM"
 FRUITS=("Apple" "Banana" "Pomo")
 
 echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
